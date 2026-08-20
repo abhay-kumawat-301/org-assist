738c6555ee2d58d1407a7fab098e03d826f58ee3
abhay-kumawat-301 <abhaykumawat301@gmail.com>
2026-08-20 15:13:46 +0530

    Fix echo commands in build.sh for proper output during build process

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "1. Building backend 🟠🟠🟠"
-npm run build --workspace=backend && "Backend build successful 🟢🟢🟢" || "Backend build failed 🔴🔴🔴"
+npm run build --workspace=backend && echo "Backend build successful 🟢🟢🟢" || echo "Backend build failed 🔴🔴🔴"
 echo "2. Building frontend 🟠🟠🟠"
-npm run build --workspace=frontend && "Frontend build successful 🟢🟢🟢" || "Frontend build failed 🔴🔴🔴"
+npm run build --workspace=frontend && echo "Frontend build successful 🟢🟢🟢" || echo "Frontend build failed 🔴🔴🔴"
